497a271c3b44b67d58c44c625f637c63ccb7722d
Neha Bagari <neha.bagari.eee11@iitbhu.ac.in>
2015-11-04 22:41:22 +0530

    Made POJO out of Article class
    Article class should contain all information regarding an article.
    The missing information is added.
    Also, all setters and getters are added, thereby making this class
    a POJO (plain old java object).
    
    Signed-off-by: Neha Bagari <neha.bagari.eee11@iitbhu.ac.in>

--- app/src/main/java/in/vshukla/thed/Article.java
+++ app/src/main/java/in/vshukla/thed/Article.java
@@ -1,31 +1,70 @@
 package in.vshukla.thed;
 
-
-
 public class Article {
-        String key;
-        String author;
-        String title;
-        public Article(String key,String author,String title)
-        {
-            this.key = key;
-            this.author = author;
-            this.title = title;
-        }
-        public String getKey()
-        {
-            return this.key;
-        }
 
+    private String key;
+    private String author;
+    private String title;
+    private String body;
+    private String kind;
+    private String date;
 
-        public String getAuthor()
-        {
-            return this.author;
-        }
-        public String getTitle()
-        {
-            return this.title;
-        }
+    public Article(String key, String author, String title, String body, String kind, String date) {
+        this.key = key;
+        this.author = author;
+        this.title = title;
+        this.body = body;
+        this.kind = kind;
+        this.date = date;
     }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
 
 
