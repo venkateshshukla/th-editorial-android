8dfe3aabc12619921eec3ed95973cb7be9185e59
Neha Bagari <neha.bagari.eee11@iitbhu.ac.in>
2015-11-05 21:36:37 +0530

    Added Internet permissions and logging
    Every good application requires proper logging. So, rather than adding logging
    features in one go at the end, I'll try logging on the go.
    Also, as interaction with web server is required, we need internet permissions.
    Added those to the manifest.
    
    Added certain files to .gitignore. (tired of seeing them in git diff).
    Deleted their cached copies.
    
    Signed-off-by: Neha Bagari <neha.bagari.eee11@iitbhu.ac.in>

--- app/src/main/java/in/vshukla/thed/Api.java
+++ app/src/main/java/in/vshukla/thed/Api.java
@@ -1,5 +1,7 @@
 package in.vshukla.thed;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 
 /**
@@ -7,18 +9,22 @@ import java.util.ArrayList;
  */
 public class Api {
 
+    public static final String TAG = "API";
     private static final String BASEURL = "http://example.com" + "/api";
 
     public ArrayList<Article> getArticleList(long timestamp) {
+        Log.d(TAG, "Getting articles after timestamp : " + String.valueOf(timestamp));
         return null;
     }
 
     public ArrayList<Article> getArticleList() {
+        Log.d(TAG, "Getting articles from past 7 days.");
         long tsLast = (System.currentTimeMillis() / 1000) - (7 * 24 * 60 * 60 );
         return this.getArticleList(tsLast);
     }
 
     public String getArticleText(String key) {
+        Log.d(TAG, "Getting articles associated with key : " + key);
         return null;
     }
 }
