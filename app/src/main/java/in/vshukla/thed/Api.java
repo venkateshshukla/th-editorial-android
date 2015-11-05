64a6a31400c950dc41691a2e0a924b368863ba23
Neha Bagari <neha.bagari.eee11@iitbhu.ac.in>
2015-11-05 23:24:51 +0530

    Implementation of API
    As we are using Volley framework, we have to play by its rules.
    Rather than returning an ArrayList of Articles, we will have to
    rely on callback functions.
    
    The workflow would be something like this.
    1. Instantiate an Object by passing the context of the Api class.
    2. Make Request.ResponseListener<JSONObject> and Request.ErrorListener objects.
       This is where the actual processing of the API response will happen.
    3. Pass these on to respective methods of Api object.
    
    Signed-off-by: Neha Bagari <neha.bagari.eee11@iitbhu.ac.in>

--- app/src/main/java/in/vshukla/thed/Api.java
+++ app/src/main/java/in/vshukla/thed/Api.java
@@ -1,8 +1,16 @@
 package in.vshukla.thed;
 
+import android.content.Context;
 import android.util.Log;
 
-import java.util.ArrayList;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by neha on 4/11/15.
@@ -12,19 +20,59 @@ public class Api {
     public static final String TAG = "API";
     private static final String BASEURL = "http://example.com" + "/api";
 
-    public ArrayList<Article> getArticleList(long timestamp) {
-        Log.d(TAG, "Getting articles after timestamp : " + String.valueOf(timestamp));
-        return null;
+    private RequestQueue requestQueue;
+
+    public Api(Context context) {
+        this.requestQueue = Volley.newRequestQueue(context);
     }
 
-    public ArrayList<Article> getArticleList() {
+    /**
+     * Get a JSONObject containing the list of articles in the given responseListener.
+     * @param timestamp The timestamp of latest available article.
+     * @param responseListener This callback function will be called on success with JSONObject containing response.
+     * @param errorListener This callback function will be called in case of error.
+     * @throws JSONException
+     */
+    public void getArticleList(long timestamp,
+                               Response.Listener<JSONObject> responseListener,
+                               Response.ErrorListener errorListener)
+            throws JSONException {
+        Log.d(TAG, "Getting articles after timestamp : " + String.valueOf(timestamp));
+        String url = BASEURL + "/list";
+        JSONObject params = new JSONObject();
+        params.put("timestamp", timestamp);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, params, responseListener, errorListener);
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    /**
+     * Get a JSONObject containing list of articles in the given responseListener. Timestamp of 7 days past is used.
+     * @param responseListener This callback function will be called on success with JSONObject containing response.
+     * @param errorListener This callback function will be called in case of error.
+     * @throws JSONException
+     */
+    public void getArticleList(Response.Listener<JSONObject> responseListener,
+                               Response.ErrorListener errorListener) throws JSONException {
         Log.d(TAG, "Getting articles from past 7 days.");
         long tsLast = (System.currentTimeMillis() / 1000) - (7 * 24 * 60 * 60 );
-        return this.getArticleList(tsLast);
+        getArticleList(tsLast, responseListener, errorListener);
     }
 
-    public String getArticleText(String key) {
+    /**
+     * Get a JSONObject containing article text in responseListener.
+     * @param key Key corresponding to an article obtained from prev API call.
+     * @param responseListener This callback function will be called on success with JSONObject containing response.
+     * @param errorListener This callback function will be called in case of error.
+     * @throws JSONException
+     */
+    public void getArticleText(String key,
+                               Response.Listener<JSONObject> responseListener,
+                               Response.ErrorListener errorListener) throws JSONException {
         Log.d(TAG, "Getting articles associated with key : " + key);
-        return null;
+        String url = BASEURL + "/news";
+        JSONObject params = new JSONObject();
+        params.put("key", key);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, params, responseListener, errorListener);
+        requestQueue.add(jsonObjectRequest);
     }
 }
