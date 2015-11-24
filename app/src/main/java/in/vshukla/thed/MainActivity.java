53d5d752692bd6070ab630106976f07029f9c070
Neha Bagari <neha.bagari.eee11@iitbhu.ac.in>
2015-11-24 22:56:38 +0530

    Run AsyncTask to get SQLiteDatabase instance
    AsyncTask is extended to make GetDatabaseTask. On acquiring a readable database,
    the task sets it to a private variable in parent class.
    
    Signed-off-by: Neha Bagari <neha.bagari.eee11@iitbhu.ac.in>

--- app/src/main/java/in/vshukla/thed/MainActivity.java
+++ app/src/main/java/in/vshukla/thed/MainActivity.java
@@ -1,16 +1,37 @@
 package in.vshukla.thed;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import in.vshukla.thed.db.DbHelper;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private SQLiteDatabase articleDb;
+
+    private void setArticleDb(SQLiteDatabase db) {
+        if (db == null) {
+            Log.w(TAG, "Setting articleDb to null.");
+        } else {
+            Log.d(TAG, "articleDb initialised.");
+        }
+        this.articleDb = db;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new GetDatabaseTask().execute(this);
     }
 
     @Override
@@ -33,5 +54,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class GetDatabaseTask extends AsyncTask <Context, Void, SQLiteDatabase> {
+        private static final String TAG = "GetDatabaseTask";
+        @Override
+        protected SQLiteDatabase doInBackground(Context... params) {
+            if (params.length != 0) {
+                Log.d(TAG, "Taking the first context passed. Acquiring readable database.");
+                SQLiteOpenHelper sqLiteOpenHelper = new DbHelper(params[0]);
+                return sqLiteOpenHelper.getReadableDatabase();
+            } else {
+                Log.w(TAG, "No contexts passed to the task. Returning null.");
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(SQLiteDatabase sqLiteDatabase) {
+            setArticleDb(sqLiteDatabase);
+        }
     }
 }
